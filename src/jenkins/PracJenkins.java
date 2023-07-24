0206064d3ea0634c5e8e23cf2f7d75a90603efee
gkankith <gkankith@gmail.com>
2023-07-24 14:44:37 +0530

    a

--- src/jenkins/PracJenkins.java
+++ src/jenkins/PracJenkins.java
@@ -1,13 +1,15 @@
 package jenkins;
 
 public class PracJenkins {
-	//push from system eclipse
+	
 	
 	public static void main(String[] args) {
 		
-		System.out.println("hello");
+		System.out.println("ankith");
 		System.out.println("welcome");
-		System.out.println("done");
+		System.out.println("hey");
+		
 	}
+
 	
 }
