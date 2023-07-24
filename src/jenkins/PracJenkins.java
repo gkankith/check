37fddfdb3cbedf2e49997dfa8371ab0abc553553
gkankith <gkankith@gmail.com>
2023-07-24 14:31:22 +0530

    1st

--- src/jenkins/PracJenkins.java
+++ src/jenkins/PracJenkins.java
@@ -1,12 +1,13 @@
 package jenkins;
 
 public class PracJenkins {
-	//push back
+	//push from system eclipse
 	
 	public static void main(String[] args) {
 		
 		System.out.println("hello");
 		System.out.println("welcome");
+		System.out.println("done");
 	}
 	
 }
